15f89d151254a96787c27816b5929cb5c31b23ef
serhii <ice2me1989@gmail.com>
2020-01-21 15:53:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.header__slider').slick({
+		arrows: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		fade: true,
+		dots: true,
+	});
+});
